d1810d0122c14368419e980f00032c040f8e06d2
ahmedatef00 <ahmedatef62437@gmail.com>
2020-11-01 01:52:59 +0200

    init Project

---
+++ resources/views/back-end/home-sections/latest-comments.blade.php
@@ -0,0 +1,24 @@
+<div class="row">
+    <div class="col-lg-12 col-md-12">
+        <div class="card">
+            <div class="card-header card-header-primary">
+                <h4 class="card-title">Latest Comments</h4>
+                <p class="card-category">Here you can see the latest comments in website</p>
+            </div>
+            <div class="card-body table-responsive">
+                <table class="table table-hover">
+                    <thead class="text-warning">
+                    <tr><th>ID</th>
+                        <th>Name</th>
+                        <th>Video</th>
+                        <th>comment</th>
+                        <th>date</th>
+                        <th>control</th>
+                    </tr></thead>
+                    <tbody>
+                    
+                    </tbody>
+                </table>
+        </div>
+    </div>
+</div>
